77d36657c1964b1303da372dbd544f985e898348
Jeff Higham <jeff@f3code.com>
2020-08-03 16:21:57 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/jeffhigham/verb_crm_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jeffhigham.verb_crm_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CrmApp());
+}
+
+class CrmApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  LandingPage({Key key}) : super(key: key);
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'CRM APP!',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
